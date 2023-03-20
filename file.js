2b090290e433deaaf364bebfdb68cdfe9ac4bd0c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-03-20 15:56:34 +0900

    1차 정지

--- file.js
+++ file.js
@@ -2,13 +2,19 @@ import fs from 'fs';
 
 let name = "parkjunhyeong";
 let date = new Date();
-// let fileName = date.getFullYear() + date.getMonth() + date.getDay() + name;
-let year = "2023";
-let month = "3";
-let day = "20";
-let fileName = year + month + day + "-" + name;
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDay();
+let fileName = year += month += day + name;
+console.log(fileName);
 
 fs.writeFileSync("./" + fileName + ".txt", name);
+// ! 파일 생성
+
+// fs.rmSync("./" + "202321parkjunhyeong.txt");
+// ! 파일 제거
+
+
 // ! 파일을 동기 방식으로 쓰다
 // ! 만든 것을 동적으로 저장할 수 있다
 // ! CRUD의 C를 해보았다.
